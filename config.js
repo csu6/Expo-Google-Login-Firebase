8922f466a63a7271328f4ba52f0654b2e1efccf6
csu6 <csuwannapak@gmail.com>
2019-06-23 00:02:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyB5C0yEhClZmhrH15bRcOE2ZZLKKyBhZfc",
-    authDomain: "finder-15062019.firebaseapp.com",
-    databaseURL: "https://finder-15062019.firebaseio.com",
-    projectId: "finder-15062019",
-    storageBucket: "finder-15062019.appspot.com",
-    messagingSenderId: "418440959152",
-    appId: "1:418440959152:web:9854cbf8c427ee63"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 
