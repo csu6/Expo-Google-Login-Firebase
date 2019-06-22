3bf84d2499e02698dec19ec98c2ae7c1b4cd05b3
csu <csuwannapak@gmail.com>
2019-06-22 23:43:29 +0200

    Google sign in

---
+++ screens/LoginScreen.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import * as Expo from 'expo';
+import { Google } from "expo"; 
+import firebase from 'firebase';
+class LoginScreen extends Component {
+  isUserEqual = (googleUser, firebaseUser) => {
+    if (firebaseUser) {
+      var providerData = firebaseUser.providerData;
+      for (var i = 0; i < providerData.length; i++) {
+        if (
+          providerData[i].providerId ===
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()
+        ) {
+          // We don't need to reauth the Firebase connection.
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+  onSignIn = googleUser => {
+    console.log('Google Auth Response', googleUser);
+    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+    var unsubscribe = firebase.auth().onAuthStateChanged(
+      function(firebaseUser) {
+        unsubscribe();
+        // Check if we are already signed-in Firebase with the correct user.
+        if (!this.isUserEqual(googleUser, firebaseUser)) {
+          // Build Firebase credential with the Google ID token.
+          var credential = firebase.auth.GoogleAuthProvider.credential(
+            googleUser.idToken,
+            googleUser.accessToken
+          );
+          // Sign in with credential from the Google user.
+          firebase
+            .auth()
+            .signInAndRetrieveDataWithCredential(credential)
+            .then(function(result) {
+              console.log('user signed in ');
+              if (result.additionalUserInfo.isNewUser) {
+                firebase
+                  .database()
+                  .ref('/users/' + result.user.uid)
+                  .set({
+                    gmail: result.user.email,
+                    profile_picture: result.additionalUserInfo.profile.picture,
+                    first_name: result.additionalUserInfo.profile.given_name,
+                    last_name: result.additionalUserInfo.profile.family_name,
+                    created_at: Date.now()
+                  })
+                  .then(function(snapshot) {
+                    // console.log('Snapshot', snapshot);
+                  });
+              } else {
+                firebase
+                  .database()
+                  .ref('/users/' + result.user.uid)
+                  .update({
+                    last_logged_in: Date.now()
+                  });
+              }
+            })
+            .catch(function(error) {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              // The email of the user's account used.
+              var email = error.email;
+              // The firebase.auth.AuthCredential type that was used.
+              var credential = error.credential;
+              // ...
+            });
+        } else {
+          console.log('User already signed-in Firebase.');
+        }
+      }.bind(this)
+    );
+  };
+  signInWithGoogleAsync = async () => {
+	  console.log("call signInWithGoogleAsync");
+    try {
+	  console.log("try signInWithGoogleAsync");
+      const result = await Expo.Google.logInAsync({
+        // androidClientId: YOUR_CLIENT_ID_HERE,
+        //behavior: 'web',
+        androidClientId: '418440959152-ensau7c7d5p4klob8gpt809v2s3sq63c.apps.googleusercontent.com',
+        //iosClientId: '', //enter ios client id
+        scopes: ['profile', 'email']
+      });
+
+	  console.log("try result.type : "+result.type);
+      if (result.type === 'success') {
+        this.onSignIn(result);
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+	  console.log("errer : "+e);
+      return { error: true };
+    }
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Sign In With Google"
+          onPress={() => this.signInWithGoogleAsync()}
+        />
+      </View>
+    );
+  }
+}
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
